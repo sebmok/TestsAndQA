e4434cb55f1d75ca7d4ab717b15dc51b3c02fe79
WSEI\s.mokwa <s.mokwa@gmail.com>
2018-09-25 19:26:40 +0200

    Dodanie nowej metody

---
+++ Bank1/Bank1/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    public class BankAccount
+    {
+        private string m_customerName;
+        private double m_balance;
+        private bool m_frozen = false;
+
+        private BankAccount() { }
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance;  }
+        }
+
+        public Debit (double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if(amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            if(amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+    }
+}
