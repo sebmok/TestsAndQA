e390879df3614362dd20beb73fc2b6004a05a001
sebmok <s.mokwa@gmail.com>
2018-10-02 18:24:33 +0200

    Add files via upload

---
+++ Math.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Math
+    {
+        private int _a;
+        private int _b;
+        public string _operation;
+
+        public int A { get { return _a; } }
+        public int B { get { return _b; } }
+
+        public Math(int a, int b, string operation)
+        {
+            _a = a;
+            _b = b;
+            _operation = operation;
+        }
+                
+        public int intPlus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int intMinus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int intMultiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int intDivision(int a, int b)
+        {
+            return a / b;
+        }
+
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddTwoNumbers_ShouldBeAdded_OK()
+        {
+           int a = 3;
+            int b = 3;
+            string Plus = "+";
+            int expected = 6;
+
+            Calculator.Math m = new Calculator.Math(a, b, Plus);
+
+            int actual = m.intPlus(a,b);
+            Assert.AreEqual(expected, actual, 0.001, "added correctly");
+
+        }
+
+        [TestMethod]
+        public void MinusTwoNumbers_ShouldBeDeminished_OK()
+        {
+            int a = 3;
+            int b = 3;
+            string Minus = "-";
+            int expected = 0;
+
+            Calculator.Math m = new Calculator.Math(a, b, Minus);
+
+            int actual = m.intMinus(a, b);
+            Assert.AreEqual(expected, actual, 0.001, "deminished correctly");
+
+        }
+
+        [TestMethod]
+        public void MultiplyTwoNumbers_ShouldBeMultiplied_OK()
+        {
+            int a = 3;
+            int b = 3;
+            string Multiply = "*";
+            int expected = 9;
+
+            Calculator.Math m = new Calculator.Math(a, b, Multiply);
+
+            int actual = m.intMultiply(a, b);
+            Assert.AreEqual(expected, actual, 0.001, "Multiplied correctly");
+
+        }
+
+        [TestMethod]
+        public void DivisionTwoNumbers_ShouldBeDivisioned_OK()
+        {
+            int a = 3;
+            int b = 3;
+            string Division = "/";
+            int expected = 1;
+
+            Calculator.Math m = new Calculator.Math(a, b, Division);
+
+            int actual = m.intDivision(a, b);
+            Assert.AreEqual(expected, actual, 0.001, "Divisioned correctly");
+
+        }
+    }
+}
+
+
+
